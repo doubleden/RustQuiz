081e313f7babb90e25744d5d9d8c629d00184091
Denis Denisov <denis.denisov8701@gmail.com>
2025-09-26 10:44:51 +0200

    Initial Commit

---
+++ RustQuiz/RustQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  RustQuizApp.swift
+//  RustQuiz
+//
+//  Created by Denis Denisov on 26/9/25.
+//
+
+import SwiftUI
+
+@main
+struct RustQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
