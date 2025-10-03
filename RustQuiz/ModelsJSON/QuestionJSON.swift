23700b4510946d309b4f3146c34279de54490b5e
Denis Denisov <denis.denisov8701@gmail.com>
2025-10-03 21:18:12 +0200

    added json file with quizes

---
+++ RustQuiz/ModelsJSON/QuestionJSON.swift
@@ -0,0 +1,36 @@
+//
+//  Question.swift
+//  RustQuiz
+//
+//  Created by Denis Denisov on 30/9/25.
+//
+
+import SwiftUI
+
+struct Question: Identifiable, Decodable {
+    let id: UUID
+    var title: String
+    var answers: [Answer]
+    
+    var descriptionText: String
+    var descriptionLink: String
+    
+    var hasUserAnswered = false
+    var isUserAnswerCorrect = false
+    
+}
+
+// MARK: - Localization
+extension Question {
+    var titleLocalized: LocalizedStringKey {
+        LocalizedStringKey(stringLiteral: title)
+    }
+    
+    var descriptionTextLocalized: LocalizedStringKey {
+        LocalizedStringKey(stringLiteral: descriptionText)
+    }
+    
+    var descriptionLinkLocalized: LocalizedStringKey {
+        LocalizedStringKey(stringLiteral: descriptionLink)
+    }
+}
