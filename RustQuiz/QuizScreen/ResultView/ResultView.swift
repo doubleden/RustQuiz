4e99c856cc240d891968cc8bc041c2c5745657d4
Denis Denisov <denis.denisov8701@gmail.com>
2025-10-27 10:13:09 +0100

    result view

--- RustQuiz/QuizScreen/ResultView/ResultView.swift
+++ RustQuiz/QuizScreen/ResultView/ResultView.swift
@@ -19,50 +19,14 @@ struct ResultView: View {
         VStack {
             Spacer()
             
-            Image("crab")
-                .resizable()
-                .scaledToFit()
-                .frame(width: screenSize.width * 0.5)
+            crabView
             
             VStack(spacing: 20) {
-                Text(title)
-                    .subFont(size: screenSize.width * 0.07)
-                    .foregroundStyle(CustomColor.titleColor.color)
+                titleText
                 
                 VStack {
-                    Text("Completed")
-                        .mainFont(size: screenSize.width * 0.08, scaleFactor: 1)
-                    
-                    ZStack {
-                        Circle()
-                            .stroke(
-                                CustomColor.backgroundColor.color,
-                                lineWidth: 10
-                            )
-                            .frame(
-                                width: screenSize.width * circleSize,
-                                height: screenSize.width * circleSize
-                            )
-                            .shadow(radius: 1)
-                        
-                        Circle()
-                            .trim(from: 0, to: CGFloat(averageRating) / 100)
-                            .stroke(
-                                CustomColor.activeColor.color,
-                                style: StrokeStyle(
-                                    lineWidth: 10,
-                                    lineCap: .round
-                                )
-                            )
-                            .frame(
-                                width: screenSize.width * circleSize,
-                                height: screenSize.width * circleSize
-                            )
-                            .rotationEffect(.degrees(-90))
-                        
-                        Text("\(averageRating)%")
-                            .mainFont(size: screenSize.width * 0.1, scaleFactor: 1)
-                    }
+                    completedText
+                    ratingCircleView
                 }
                 .foregroundStyle(CustomColor.activeColor.color)
             }
@@ -71,19 +35,75 @@ struct ResultView: View {
             
             Spacer()
             
-            VStack {
-                Button(action: backToMenuAction) {
-                    Text("Back to menu")
-                        .mainButtonShape(
-                            fontColor: CustomColor.backgroundColor.color,
-                            backgroundColor: CustomColor.activeColor.color
-                        )
-                }
-                .buttonStyle(ButtonPressInStyle())
-            }
-            .padding()
+            backToMenuButtonView
         }
         .mainBackground()
+    }
+}
+
+// MARK: - Views
+private extension ResultView {
+    var crabView: some View {
+        Image("crab")
+            .resizable()
+            .scaledToFit()
+            .frame(width: screenSize.width * 0.5)
+    }
+    
+    var titleText: some View {
+        Text(title)
+            .subFont(size: screenSize.width * 0.07)
+            .foregroundStyle(CustomColor.titleColor.color)
+    }
+    
+    var completedText: some View {
+        Text("Completed")
+            .mainFont(size: screenSize.width * 0.08, scaleFactor: 1)
+    }
+    
+    var ratingCircleView: some View {
+        ZStack {
+            Circle()
+                .stroke(
+                    CustomColor.backgroundColor.color,
+                    lineWidth: 10
+                )
+                .frame(
+                    width: screenSize.width * circleSize,
+                    height: screenSize.width * circleSize
+                )
+                .shadow(radius: 1)
+            
+            Circle()
+                .trim(from: 0, to: CGFloat(averageRating) / 100)
+                .stroke(
+                    CustomColor.activeColor.color,
+                    style: StrokeStyle(
+                        lineWidth: 10,
+                        lineCap: .round
+                    )
+                )
+                .frame(
+                    width: screenSize.width * circleSize,
+                    height: screenSize.width * circleSize
+                )
+                .rotationEffect(.degrees(-90))
+            
+            Text("\(averageRating)%")
+                .mainFont(size: screenSize.width * 0.1, scaleFactor: 1)
+        }
+    }
+    
+    var backToMenuButtonView: some View {
+        Button(action: backToMenuAction) {
+            Text("Back to menu")
+                .mainButtonShape(
+                    fontColor: CustomColor.backgroundColor.color,
+                    backgroundColor: CustomColor.activeColor.color
+                )
+        }
+        .buttonStyle(ButtonPressInStyle())
+        .padding()
     }
 }
 
